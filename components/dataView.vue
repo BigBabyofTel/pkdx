47c6e855fbbdfeca5b17c2a3a501255c0863d47d
BigBabyofTel <augustus.tb@outlook.com>
2025-12-14 16:29:08 -0600

    pokemon information display fixed

--- components/dataView.vue
+++ components/dataView.vue
@@ -11,9 +11,9 @@ defineProps({
 </script>
 
 <template>
-  <el-card class="row-span-3 col-start-3 col-end-7 backdrop-blur-xl bg-slate-900/20 border border-cyan-400/40 shadow-2xl">
-    <p v-for="type in dataEntry.flavor_text_entries" :key="type.flavor_text">
-      {{ type.flavor_text }}
+  <el-card class="row-span-3 col-start-3 col-end-7 backdrop-blur-xl bg-slate-900/20 border border-cyan-400/40 shadow-2xl flex items-center justify-center">
+    <p class="text-3xl text-center">
+      {{ dataEntry?.flavor_text_entries?.[1]?.flavor_text || 'No description available' }}
     </p>
   </el-card>
 </template>
